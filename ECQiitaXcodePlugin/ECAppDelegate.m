ee95824f5b80e6be6e0282d33d3c3d67c31e3444
Daisuke Sato <g00.eccyan@spinningworks.com>
2012-10-10 13:09:45 +0900

    Initial Commit

---
+++ ECQiitaXcodePlugin/ECAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  ECAppDelegate.m
+//  ECQiitaXcodePlugin
+//
+//  Created by 佐藤大資 on 12/10/10.
+//  Copyright (c) 2012年 佐藤大資. All rights reserved.
+//
+
+#import "ECAppDelegate.h"
+
+@implementation ECAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ ECQiitaXcodePlugin/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ECQiitaXcodePlugin
+//
+//  Created by 佐藤大資 on 12/10/10.
+//  Copyright (c) 2012年 佐藤大資. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
